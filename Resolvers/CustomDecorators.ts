baa434793e343754b8e210854ed0ba64c91d66fa
Ghyath <ghyath.darwish@gmail.com>
2021-08-18 20:42:27 +0300

    Change auth property from 'authenticated' to 'payload'

--- Resolvers/CustomDecorators.ts
+++ Resolvers/CustomDecorators.ts
@@ -3,7 +3,7 @@ import { Context } from "../@types/types";
 
 export function Guest() {
     return createMethodDecorator(async ({context}:{context:Context}, next) => {
-      if(context?.ctx?.req?.authenticated){
+      if(context?.ctx?.req?.payload){
         throw Error("You already loggedIn")
       }  
       return next();
