e774d4ec465ccd2ea61839991193be74ba318877
Ghyath <ghyath.darwish@gmail.com>
2021-08-22 01:00:33 +0300

    add rateLimit decorator

--- Resolvers/CustomDecorators.ts
+++ Resolvers/CustomDecorators.ts
@@ -1,6 +1,5 @@
-import { createMethodDecorator } from "type-graphql";
+import { createMethodDecorator, ResolverData, UseMiddleware } from "type-graphql";
 import { Context } from "../@types/types";
-
 export function Guest() {
     return createMethodDecorator(async ({context}:{context:Context}, next) => {
       if(context?.ctx?.req?.payload){
@@ -8,4 +7,31 @@ export function Guest() {
       }  
       return next();
     });
+}
+export default function RateLimit({window, max, errorMessage}:{window:number,max:number,errorMessage?:string}) {
+
+  return UseMiddleware(async ({ info:{variableValues, fieldName} , context}: {context:Context,info:any}, next) => {
+      let visitorKey = context.userId ? "user:"+context.userId: "ip:"+context?.ctx?.req?.ip;
+      const quickStore = context.ctx.req.quickStore
+      // const variableKey = limitByVariables &&
+      //     JSON.stringify(variableValues)
+      //         .replace(/[^a-zA-Z0-9,]/g,"")
+      //         .trim();
+      visitorKey = visitorKey.replace(/:/ig,"-")
+      const key:string = ["limit", fieldName, visitorKey].join("-");
+      
+      const oldRecord = await quickStore.get(key)
+      if(oldRecord) {
+        if(parseInt(oldRecord) > max){
+            throw new Error(errorMessage || 'Rate Limit Exceeded')
+        }else {
+          quickStore.inc(key)
+        }
+      }else{
+        console.log(window);
+        
+        quickStore.set(key, "1", window);
+      }
+      return next();
+  });
 }
