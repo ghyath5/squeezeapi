da0af08ba5837ddae75b085976ed58d54bdd8334
Ghyath <ghyath.darwish@gmail.com>
2021-08-21 23:01:56 +0300

    Fix verifying phoneNumber by checking on botth code and number

--- Resolvers/Auth/index.ts
+++ Resolvers/Auth/index.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 import { ExpressContext } from 'apollo-server-express';
 import {  Arg, Authorized, Ctx, Extensions, Mutation, Resolver} from 'type-graphql'
+import { Context } from '../../@types/types';
 import { sufficientRoles } from '../../utils/auth';
 import { Guest } from '../CustomDecorators';
 import {  AuthResponse, LoginInputData, RegisterInputData, VerifyLoginResponse } from './types';
@@ -34,7 +35,7 @@ export class Auth {
     });
     // if(!user.phoneNumberConfirmedAt){
       // send verification code
-    ctx.req.actions?.sendLoginCode(user.id)
+    ctx.req.actions?.sendLoginCode(user.phoneNumber,user.id)
     // }
     return {
       message:'Code sent',
@@ -47,7 +48,7 @@ export class Auth {
   @Mutation(()=>AuthResponse)
   async login(
     @Arg("LoginInputData") data:LoginInputData,
-    @Ctx() {prisma,ctx}:{prisma:PrismaClient,ctx:ExpressContext}
+    @Ctx() {prisma,ctx}:Context
   ):Promise<AuthResponse>{
     const {phoneNumber} = data
     const login = phoneNumber
@@ -79,15 +80,29 @@ export class Auth {
   @Mutation(()=>VerifyLoginResponse)
   async verifyOTP(
     @Arg("OTPcode") code:string,
-    @Ctx() {prisma,ctx}:{prisma:PrismaClient,ctx:ExpressContext}
-  ):Promise<VerifyLoginResponse>{    
-    let storedCode = await ctx?.req?.quickStore.get(`phone_number_verification_code:${ctx.req?.payload?.userId}`)
-    if(storedCode != code){
+    @Ctx() {prisma,ctx,userId}:Context
+  ):Promise<VerifyLoginResponse>{
+    let storedCode = await ctx?.req?.quickStore.get(`phone_number_verification_code:${userId}`)
+    if(!storedCode)throw new Error("Incorrect code")
+
+    const payloadCode = JSON.parse(storedCode)
+    
+    if(!payloadCode.code || payloadCode.code !== code || !code){
       throw new Error("Incorrect code")
     }
+    prisma.user.update({
+      where:{
+        id:userId
+      },
+      data:{
+        loginConfirmedAt:{
+          set:new Date()
+        }
+      }
+    })
     ctx?.req?.actions?.sendTokens({
       roles:['USER'],
-      userId:ctx.req?.payload?.userId
+      userId
     })
     // let user = await prisma.user.findUnique({where:{id:ctx?.req?.payload?.userId}}) as User
     return {
@@ -100,10 +115,16 @@ export class Auth {
   @Authorized("UNCONFIRMED")
   @Mutation(()=>VerifyLoginResponse)
   async resendOTP(
-    @Ctx() {prisma,ctx}:{prisma:PrismaClient,ctx:ExpressContext}
+    @Ctx() {prisma,ctx,userId}:Context
   ):Promise<VerifyLoginResponse>{
-    let userId = ctx?.req?.payload?.userId
-    ctx.req.actions?.sendLoginCode(userId)
+    const user = await prisma.user.findUnique({
+      where:{id:userId}
+    })
+    if(!user){
+      ctx.req?.actions?.destoryTokens()
+      throw new Error("User not found")
+    }
+    ctx.req.actions?.sendLoginCode(user.phoneNumber)
     return {
       message:'Code sent',
       isSuccess:true
