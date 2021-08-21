65c2c59cd7f92eccaa1da47be55e249067a3de20
Ghyath <ghyath.darwish@gmail.com>
2021-08-22 01:00:57 +0300

    add rate limit to services resolver

--- Resolvers/Services/index.ts
+++ Resolvers/Services/index.ts
@@ -1,7 +1,12 @@
 import { Authorized, Extensions } from "type-graphql";
-import {ServiceRelationsResolver,FindManyServiceResolver} from "../../prisma/generated/typegraphql";
+import {ServiceRelationsResolver,FindManyServiceResolver, applyResolversEnhanceMap} from "../../prisma/generated/typegraphql";
 import { sufficientRoles } from "../../utils/auth";
+import RateLimit from "../CustomDecorators";
 
-
+applyResolversEnhanceMap({
+    Service:{
+        services:[RateLimit({window:30,max:2})]
+    }
+})
 
 export default [FindManyServiceResolver,ServiceRelationsResolver]
