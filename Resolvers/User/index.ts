b7fc3be793ff341ef122ce1b7965b74dd9ce35a3
Ghyath <ghyath.darwish@gmail.com>
2021-08-21 23:00:23 +0300

    Add changeNumber with verifyCode

--- Resolvers/User/index.ts
+++ Resolvers/User/index.ts
@@ -1,21 +1,29 @@
 import { PrismaClient } from "@prisma/client";
 import { ExpressContext } from "apollo-server-express/dist/ApolloServer";
-import {  Authorized, Ctx, Extensions, Query, Resolver, UseMiddleware } from "type-graphql";
+import {  Arg, Authorized, Ctx, Extensions, Mutation, NonEmptyArray, Query, Resolver, UseMiddleware } from "type-graphql";
+import { Context } from "../../@types/types";
 import { AuthorizeSelf } from "../../middlewares";
-import { Role, User,UpdateUserResolver,applyResolversEnhanceMap } from "../../prisma/generated/typegraphql";
-
+import { Role, User,UpdateUserResolver,applyResolversEnhanceMap, applyInputTypesEnhanceMap, applyModelsEnhanceMap } from "../../prisma/generated/typegraphql";
 import { sufficientRoles } from "../../utils/auth";
-
+import { applyDecoratorsFields } from "../../utils/helpers";
+import { ChangeNumberResponse, UpdatePhoneNumberInput } from "./types";
+const ACCESS_FIELDS_ROLES = ['ADMIN','MANAGER']
 
 applyResolversEnhanceMap({
   User:{
-    updateUser:[Authorized(['USER']),UseMiddleware(AuthorizeSelf),Extensions({check:(isLoggedIn,roles)=>isLoggedIn&&!sufficientRoles(['UNCONFIRMED'],roles)})]
+    updateUser:[Authorized(['USER',...ACCESS_FIELDS_ROLES]),UseMiddleware(AuthorizeSelf),Extensions({check:(isLoggedIn,roles)=>isLoggedIn&&!sufficientRoles(['UNCONFIRMED'],roles)})]
   }
 })
 
+const hiddenInputFields = applyDecoratorsFields(['phoneNumber','loginConfirmedAt','updatedAt','createdAt','role','id'],Extensions({hide:true}))
+applyInputTypesEnhanceMap({
+  UserUpdateInput:{
+    fields:hiddenInputFields
+  }
+})
 
 @Resolver()
-export class Me extends UpdateUserResolver{
+export class Queries extends UpdateUserResolver{
   @Query(()=>String)
   root():String{
     return 'Root';
@@ -26,6 +34,64 @@ export class Me extends UpdateUserResolver{
   async me(@Ctx() {prisma,ctx}:{prisma:PrismaClient,ctx:ExpressContext}):Promise<User>{
     let me = await prisma.user.findUnique({where:{id:ctx.req.payload.userId}}) as User
     return me;
-  }  
+  }
 }
 
+@Resolver()
+export class Mutations {
+  @Authorized(Role.USER)
+  @Mutation(()=>ChangeNumberResponse)
+  async updatePhoneNumber(
+    @Arg("newPhoneNumber") phoneNumber:string,
+    @Ctx() {prisma,ctx}:{prisma:PrismaClient,ctx:ExpressContext}
+  ):Promise<ChangeNumberResponse>{
+    let user = await prisma.user.findUnique({
+      where:{
+        phoneNumber
+      }
+    })
+    if(user){
+      throw new Error('phone number already in use')
+    }
+    ctx?.req.actions?.sendLoginCode(phoneNumber)
+    return {
+      isSuccess:true,
+      message:'code sent'
+    };
+  }
+
+  @Authorized(Role.USER)
+  @Mutation(()=>User)
+  async verifyPhoneNumberChange(
+    @Arg("UpdateInput") updatePhoneInputs:UpdatePhoneNumberInput,
+    @Ctx() {prisma,ctx,userId}:Context
+  ):Promise<User>{
+    const {code,phoneNumber} = updatePhoneInputs
+    let storedCode = await ctx?.req?.quickStore.get(`phone_number_verification_code:${userId}`)
+    if(!storedCode) throw new Error("Incorrect code")
+
+    const payloadCode = JSON.parse(storedCode)
+    if(payloadCode.phoneNumber !== phoneNumber || !phoneNumber){
+      throw new Error("Incorrect phoneNumber")
+    }
+    if(!payloadCode.code || payloadCode.code !== code || !code){
+      throw new Error("Incorrect code")
+    }
+    return await prisma.user.update({
+      where:{
+        id:userId
+      },
+      data:{
+        loginConfirmedAt:{
+          set:new Date()
+        },
+        phoneNumber:{
+          set:phoneNumber
+        }
+      }
+    })
+  }
+}
+
+
+export default [Mutations,Queries] as NonEmptyArray<Function>
--- Resolvers/index.ts
+++ Resolvers/index.ts
@@ -1,5 +1,5 @@
 import {   NonEmptyArray} from 'type-graphql'
 import { Auth } from './Auth';
 import Service from './Services';
-import {Me} from './User';
-export default [Me,Auth,...Service] as NonEmptyArray<Function>
+import UserResolvers from './User';
+export default [Auth,...UserResolvers,...Service] as NonEmptyArray<Function>
