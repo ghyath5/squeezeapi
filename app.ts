fb176aa2e334cea2ccd4423b08f403cda0cbbc0c
Ghyath <ghyath.darwish@gmail.com>
2021-08-24 19:23:18 +0300

    add query depth limitaion

--- app.ts
+++ app.ts
@@ -18,7 +18,8 @@ import { Context } from "./@types/types";
 import Actions from "./Resolvers/Auth/actions";
 import { PruneSchema, wrapSchema } from '@graphql-tools/wrap';
 import { FilterSchema } from "./limitIntrospection";
-
+import { fieldExtensionsEstimator, getComplexity, simpleEstimator } from "graphql-query-complexity";
+import depthLimit from 'graphql-depth-limit'
 const prisma = new PrismaClient();
 const middleware = async (resolve, root, args, context, info: GraphQLResolveInfo) => {
   const result = new PrismaSelect(info).value;
@@ -57,10 +58,53 @@ async function startApolloServer() {
     introspection:process.env.NODE_ENV !== 'production',
     schema,
     context: (ctx):Context => ({ prisma,ctx,userId:ctx?.req?.payload?.userId}),
+    validationRules: [ depthLimit(3) ],
     plugins: [
       process.env.NODE_ENV === 'production' ?
       ApolloServerPluginLandingPageProductionDefault({ footer: false }) :
-      ApolloServerPluginLandingPageLocalDefault({ footer: false })
+      ApolloServerPluginLandingPageLocalDefault({ footer: false }),
+
+      {
+        requestDidStart: async () => ({
+          async didResolveOperation({ request, document }) {
+            /**
+             * This provides GraphQL query analysis to be able to react on complex queries to your GraphQL server.
+             * This can be used to protect your GraphQL servers against resource exhaustion and DoS attacks.
+             * More documentation can be found at https://github.com/ivome/graphql-query-complexity.
+             */
+            const complexity = getComplexity({
+              // Our built schema
+              schema,
+              // To calculate query complexity properly,
+              // we have to check only the requested operation
+              // not the whole document that may contains multiple operations
+              operationName: request.operationName,
+              // The GraphQL query document
+              query: document,
+              // The variables for our GraphQL query
+              variables: request.variables,
+              // Add any number of estimators. The estimators are invoked in order, the first
+              // numeric value that is being returned by an estimator is used as the field complexity.
+              // If no estimator returns a value, an exception is raised.
+              estimators: [
+                // Using fieldExtensionsEstimator is mandatory to make it work with type-graphql.
+                fieldExtensionsEstimator(),
+                // Add more estimators here...
+                // This will assign each field a complexity of 1
+                // if no other estimator returned a value.
+                simpleEstimator({ defaultComplexity: 1 }),
+              ],
+            });
+            // Here we can react to the calculated complexity,
+            // like compare it with max and throw error when the threshold is reached.
+            if (complexity > 20) {
+              throw new Error(
+                `Sorry, too complicated query! ${complexity} is over 20 that is the max allowed complexity.`,
+              );
+            }
+          },
+        }),    
+      },
     ]
   });
   await server.start();
