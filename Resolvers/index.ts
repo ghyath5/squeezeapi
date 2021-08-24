c33e662c01548b11c62282db26ef45b2c2ba808a
Ghyath <ghyath.darwish@gmail.com>
2021-08-24 19:26:14 +0300

    Migrate enhanced resolvers to separated file

--- Resolvers/index.ts
+++ Resolvers/index.ts
@@ -2,4 +2,5 @@ import {   NonEmptyArray} from 'type-graphql'
 import { Auth } from './Auth';
 import Service from './Services';
 import UserResolvers from './User';
+import './EnhanceResolvers'
 export default [Auth,...UserResolvers,...Service] as NonEmptyArray<Function>
