0da4f7f3940da781372413fde274152d64471b12
Ghyath <ghyath.darwish@gmail.com>
2021-08-26 17:38:25 +0300

    Prettify code

--- Resolvers/EnhanceResolvers.ts
+++ Resolvers/EnhanceResolvers.ts
@@ -1,26 +1,40 @@
-import { Authorized, UseMiddleware, Extensions } from "type-graphql"
-import { AuthorizeSelf } from "../middlewares"
-import { applyInputTypesEnhanceMap, applyRelationResolversEnhanceMap, applyResolversEnhanceMap } from "../prisma/generated/typegraphql"
-import { sufficientRoles } from "../utils/auth"
-import { applyDecoratorsFields } from "../utils/helpers"
+import { Authorized, UseMiddleware, Extensions } from "type-graphql";
+import { AuthorizeSelf } from "../middlewares";
+import {
+  applyInputTypesEnhanceMap,
+  applyRelationResolversEnhanceMap,
+  applyResolversEnhanceMap,
+} from "../prisma/generated/typegraphql";
+import { sufficientRoles } from "../utils/auth";
+import { applyDecoratorsFields } from "../utils/helpers";
 
-const ACCESS_FIELDS_ROLES = ['ADMIN','MANAGER']
+const ACCESS_FIELDS_ROLES = ["ADMIN", "MANAGER"];
 
 applyResolversEnhanceMap({
-  User:{
-    updateUser:[Authorized(['USER',...ACCESS_FIELDS_ROLES]),UseMiddleware(AuthorizeSelf),Extensions({check:(isLoggedIn,roles)=>isLoggedIn&&!sufficientRoles(['UNCONFIRMED'],roles)})]
-  }
-})
+  User: {
+    updateUser: [
+      Authorized(["USER", ...ACCESS_FIELDS_ROLES]),
+      UseMiddleware(AuthorizeSelf),
+      Extensions({
+        check: (isLoggedIn, roles) =>
+          isLoggedIn && !sufficientRoles(["UNCONFIRMED"], roles),
+      }),
+    ],
+  },
+});
 
-const hiddenInputFields = applyDecoratorsFields(['phoneNumber','loginConfirmedAt','updatedAt','createdAt','role','id'],Extensions({hide:true}))
+const hiddenInputFields = applyDecoratorsFields(
+  ["phoneNumber", "loginConfirmedAt", "updatedAt", "createdAt", "role", "id"],
+  Extensions({ hide: true })
+);
 applyInputTypesEnhanceMap({
-  UserUpdateInput:{
-    fields:hiddenInputFields
-  }
-})
+  UserUpdateInput: {
+    fields: hiddenInputFields,
+  },
+});
 
 applyRelationResolversEnhanceMap({
-  Step:{
-    services:[Extensions({hide:true})]
-  }
-})
+  Step: {
+    services: [Extensions({ hide: true })],
+  },
+});
