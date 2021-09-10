bf18e8b5b3907a4b87417d8c36521bc492aec2c5
Ghyath <ghyath.darwish@gmail.com>
2021-09-11 00:06:25 +0300

    add prismaSelect plugin

--- Resolvers/Global/User/index.ts
+++ Resolvers/Global/User/index.ts
@@ -1,16 +1,22 @@
+import { PrismaSelect } from "@paljs/plugins";
 import { PrismaClient } from "@prisma/client";
 import { ExpressContext } from "apollo-server-express/dist/ApolloServer";
-import {  Ctx, Extensions, NonEmptyArray, Query, Resolver } from "type-graphql";
+import {  Ctx, Extensions, Info, NonEmptyArray, Query, Resolver } from "type-graphql";
 import {  User,UserRelationsResolver} from "../../../prisma/generated/typegraphql";
 import { sufficientRoles } from "../../../utils/auth";
-
-
 @Resolver()
 export class Queries extends UserRelationsResolver{
   @Extensions({check:(isLoggedIn,roles)=>isLoggedIn&&!sufficientRoles(['UNCONFIRMED'],roles)})
   @Query(()=>User)
-  async me(@Ctx() {prisma,ctx}:{prisma:PrismaClient,ctx:ExpressContext}):Promise<User>{
-    let me = await prisma.user.findUnique({where:{id:ctx.req.payload.userId}}) as User
+  async me(
+    @Ctx() {prisma,ctx}:{prisma:PrismaClient,ctx:ExpressContext},
+    @Info() info
+  ):Promise<User>{
+    const select = new PrismaSelect(info).value;
+    let me = await prisma.user.findUnique({
+      where:{id:ctx.req.payload.userId},
+      ...select
+    }) as User
     return me;
   }
 }
