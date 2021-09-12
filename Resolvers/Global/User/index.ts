e2c5c421b65db8b7cf9c31b3ef32fedc51f75ef6
Ghyath <ghyath.darwish@gmail.com>
2021-09-12 13:49:53 +0300

    Support selected fields

--- Resolvers/Global/User/index.ts
+++ Resolvers/Global/User/index.ts
@@ -1,22 +1,22 @@
-import { PrismaSelect } from "@paljs/plugins";
 import { PrismaClient } from "@prisma/client";
 import { ExpressContext } from "apollo-server-express/dist/ApolloServer";
-import {  Ctx, Extensions, Info, NonEmptyArray, Query, Resolver } from "type-graphql";
+import { Ctx, Extensions, NonEmptyArray, Query, Resolver } from "type-graphql";
 import {  User,UserRelationsResolver} from "../../../prisma/generated/typegraphql";
 import { sufficientRoles } from "../../../utils/auth";
+import { Fields } from "../../CustomDecorators";
+
 @Resolver()
 export class Queries extends UserRelationsResolver{
   @Extensions({check:(isLoggedIn,roles)=>isLoggedIn&&!sufficientRoles(['UNCONFIRMED'],roles)})
   @Query(()=>User)
   async me(
+    @Fields() fields,
     @Ctx() {prisma,ctx}:{prisma:PrismaClient,ctx:ExpressContext},
-    @Info() info
   ):Promise<User>{
-    const select = new PrismaSelect(info).value;
     let me = await prisma.user.findUnique({
       where:{id:ctx.req.payload.userId},
-      ...select
-    }) as User
+      ...fields
+    }) as User    
     return me;
   }
 }
