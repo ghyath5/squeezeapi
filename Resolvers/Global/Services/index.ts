0551909c940bd256627b19fdfd71ae5b8979c37a
Ghyath <ghyath.darwish@gmail.com>
2021-09-14 20:39:19 +0300

    Customize option settings

--- Resolvers/Global/Services/index.ts
+++ Resolvers/Global/Services/index.ts
@@ -1,3 +1,19 @@
-import {FindManyServiceResolver, StepRelationsResolver,FindManyStepResolver, StepOptionRelationsResolver} from "../../../prisma/generated/typegraphql";
 
-export default [FindManyServiceResolver,StepRelationsResolver,FindManyStepResolver,StepOptionRelationsResolver]
+import { Ctx, FieldResolver, Resolver, Root } from "type-graphql";
+import { Context } from "../../../@types/types";
+import {FindManyServiceResolver, StepRelationsResolver,FindManyStepResolver, StepOptionRelationsResolver, StepOption} from "../../../prisma/generated/typegraphql";
+import { customizeOptionsSettings } from "./utils";
+
+
+@Resolver(of => StepOption)
+class Options {
+    @FieldResolver(()=>Object)
+    settings(
+        @Root() option: StepOption,
+        @Ctx() ctx : Context
+    ):Object {        
+        return customizeOptionsSettings(option,ctx)
+    }
+}
+
+export default [FindManyServiceResolver,StepRelationsResolver,FindManyStepResolver,StepOptionRelationsResolver,Options]
