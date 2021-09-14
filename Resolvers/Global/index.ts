017ee2b9921d267b19b86089b76c24ab2d049713
Ghyath <ghyath.darwish@gmail.com>
2021-09-14 20:41:29 +0300

    Remove orderResolvers

--- Resolvers/Global/index.ts
+++ Resolvers/Global/index.ts
@@ -1,5 +1,7 @@
 import {NonEmptyArray} from 'type-graphql'
 import Service from './Services';
 import UserResolvers from './User';
-import OrderResolvers from './Order';
-export default [...OrderResolvers,...UserResolvers,...Service] as NonEmptyArray<Function>
+// import OrderResolvers from './Order';
+export default [
+    // ...OrderResolvers,
+    ...UserResolvers,...Service] as NonEmptyArray<Function>
